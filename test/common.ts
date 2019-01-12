88b3efaa1e105b9ae61ac207be7c6d88902b58f7
Raiondesu <a.raiondesu@gmail.com>
2019-01-12 14:54:06 +0300

    Add [test/common]: add rejecting import factory

--- test/common.ts
+++ test/common.ts
@@ -1,3 +1,7 @@
 export function importFactory(path: string) {
   return Promise.resolve(require('./modules/' + path));
 }
+
+export function wrongImportFactory(path: string) {
+  return Promise.reject(new Error(path));
+}
