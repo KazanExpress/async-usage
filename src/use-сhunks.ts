797cba4285905b8d80139914d159f50a2d3f19b4
Raiondesu <a.raiondesu@gmail.com>
2019-09-11 12:37:31 +0300

    Refactor [replaceNonKebabSymbols -> replaceInvalidSymbols] use-chunks

--- src/use-сhunks.ts
+++ src/use-сhunks.ts
@@ -1,7 +1,7 @@
 import { ChunkImporter, ChunkImportOptions, ChunkPromiseMap, ChunkImportMap, Chunk } from './types';
 import { isStr } from './util';
 
-const replaceNonKebabSymbols = (str: string) => str.replace(/[^\w\d-_]/gi, '-');
+const replaceInvalidSymbols = (str: string) => str.replace(/[^\w\d-_]/gi, '-');
 
 export function useChunks<C extends Chunk>(
   importChunk: ChunkImporter<C>,
@@ -12,7 +12,7 @@ export function useChunks<C extends Chunk>(
     return Object.keys(chunksMap).reduce<ChunkPromiseMap<C>>(
       (obj, name) => {
         const chunk = chunksMap[name];
-        const safeName = replaceNonKebabSymbols(name);
+        const safeName = replaceInvalidSymbols(name);
 
         if (isStr(chunk)) {
           obj[safeName] = importChunk(chunk.replace('*', name), relativePath);
@@ -28,7 +28,7 @@ export function useChunks<C extends Chunk>(
   return useChunks(importChunk, chunksMap.reduce(
     (obj: ChunkImportMap, name: string | ChunkImportMap) => {
       if (isStr(name)) {
-        obj[replaceNonKebabSymbols(name)] = name;
+        obj[replaceInvalidSymbols(name)] = name;
 
         return obj;
       } else {
